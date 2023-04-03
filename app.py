a689b224169f8c7e8c2e097a2983bac30a9a4faf
erdenay <erdenay@Ubuntu>
2023-04-03 14:09:37 +0200

    auto pod log refresh issue fixed

--- app.py
+++ app.py
@@ -10,19 +10,11 @@ from apscheduler.triggers.interval import IntervalTrigger
 from threading import Lock
 from kubernetes.client.exceptions import ApiException
 
-urllib3.disable_warnings()
-
 # Configure Kubernetes API client
 config.load_incluster_config()
-api_client = client.ApiClient()
 
 # Initialize Flask app
 app = Flask(__name__, static_folder="static")
-
-# Connection pooling for Kubernetes API client
-api_client.rest_client.pool_manager = urllib3.PoolManager(num_pools=10, maxsize=10, retries=False, timeout=urllib3.Timeout(connect=1, read=2))
-
-error_check_enabled = True
 
 @app.route("/")
 def index():
@@ -31,26 +23,76 @@ def index():
     node_metrics = get_node_metrics()
     node_metrics_human_readable = process_node_metrics(node_metrics)
 
-    return render_template("index.html", error_check_enabled=error_check_enabled, deployments=deployments,
+    return render_template("index.html", deployments=deployments,
                            deployment_pods=deployment_pods,
                            node_metrics=node_metrics_human_readable)
 
-@lru_cache(maxsize=None)
+
+@app.route("/scale", methods=["POST"])
+def scale():
+    namespace = request.form["namespace"]
+    deployment_name = request.form["deployment_name"]
+    replicas = int(request.form["replicas"])
+
+    scale_deployment(namespace, deployment_name, replicas)
+    return redirect("/")
+
+@app.route("/logs", methods=["POST"])
+def logs():
+    namespace = request.form["namespace"]
+    pod_name = request.form["pod_name"]
+
+    logs = get_pod_logs(namespace, pod_name)
+    return render_template("logs.html", logs=logs)
+
+@app.route("/fetch_logs", methods=["POST"])
+def fetch_logs():
+    namespace = request.form["namespace"]
+    pod_name = request.form["pod_name"]
+
+    logs = get_pod_logs(namespace, pod_name)
+    return logs
+
+@app.route("/delete-error-completed-pods", methods=["POST"])
+def delete_error_completed_pods():
+    delete_error_and_completed_pods()
+    return redirect("/")
+
+
 def get_deployments():
     api_instance = client.AppsV1Api()
     deployments = api_instance.list_deployment_for_all_namespaces()
     return deployments
 
+def scale_deployment(namespace, deployment_name, replicas):
+    api_instance = client.AppsV1Api()
 
-def get_deployment_pods(deployments):
-    deployment_pods = {}
-    for deployment in deployments.items:
-        namespace = deployment.metadata.namespace
-        deployment_name = deployment.metadata.name
-        deployment_pods[deployment_name] = get_pods_by_deployment(namespace, deployment_name)
-    return deployment_pods
+    # Update the deployment with the new replica count
+    update_deployment = {
+        'spec': {
+            'replicas': replicas
+        }
+    }
 
-@lru_cache(maxsize=None)
+    try:
+        api_response = api_instance.patch_namespaced_deployment(deployment_name, namespace, update_deployment)
+        print(f"Deployment {deployment_name} in namespace {namespace} has been scaled to {replicas} replicas.")
+    except client.ApiException as e:
+        print(f"Exception when calling AppsV1Api->patch_namespaced_deployment: {e}")
+
+def get_pod_logs(namespace, pod_name):
+    api_instance = client.CoreV1Api()
+    logs = api_instance.read_namespaced_pod_log(pod_name, namespace)
+    return logs
+
+def delete_error_and_completed_pods():
+    api_instance = client.CoreV1Api()
+    pods = api_instance.list_pod_for_all_namespaces()
+
+    for pod in pods.items:
+        if pod.status.phase in ['Error', 'Succeeded', 'Failed']:
+            api_instance.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
+
 def get_pods_by_deployment(namespace, deployment_name):
     api_instance = client.CoreV1Api()
 
@@ -63,7 +105,37 @@ def get_pods_by_deployment(namespace, deployment_name):
     pods = api_instance.list_namespaced_pod(namespace, label_selector=label_selector)
     return pods
 
-@lru_cache(maxsize=None)
+def get_deployment_pods(deployments):
+    deployment_pods = {}
+    for deployment in deployments.items:
+        namespace = deployment.metadata.namespace
+        deployment_name = deployment.metadata.name
+        deployment_pods[deployment_name] = get_pods_by_deployment(namespace, deployment_name)
+    return deployment_pods
+
+@app.route("/restart", methods=["POST"])
+def rollout_restart_deployment():
+    namespace = request.form["namespace"]
+    deployment_name = request.form["deployment_name"]
+    rollout_restart(namespace, deployment_name)  # Call the rollout_restart function
+    return redirect("/")
+
+
+def rollout_restart(namespace, deployment_name):
+    api_instance = client.AppsV1Api()
+    body = {
+        "spec": {
+            "template": {
+                "metadata": {
+                    "annotations": {
+                        "kubectl.kubernetes.io/restartedAt": datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
+                    }
+                }
+            }
+        }
+    }
+    api_instance.patch_namespaced_deployment(deployment_name, namespace, body)
+
 def get_node_metrics():
     api_instance = client.CustomObjectsApi()
     group = 'metrics.k8s.io'
@@ -95,8 +167,7 @@ def get_node_metrics():
     except ApiException as e:
         print(f"Exception when calling CustomObjectsApi->list_cluster_custom_object: {e}")
         return None
-
-
+    
 def process_node_metrics(node_metrics):
     node_metrics_human_readable = []
 
@@ -124,185 +195,11 @@ def process_node_metrics(node_metrics):
 
     return node_metrics_human_readable
 
-
-@app.route("/scale", methods=["POST"])
-def scale():
-    namespace = request.form["namespace"]
-    deployment_name = request.form["deployment_name"]
-    replicas = int(request.form["replicas"])
-
-    scale_deployment(namespace, deployment_name, replicas)
-
-    # Clear the cache to force updates
-    get_deployments.cache_clear()
-    get_pods_by_deployment.cache_clear()
-    get_node_metrics.cache_clear()
-
-    return redirect("/")
-
-@lru_cache(maxsize=None)
-def scale_deployment(namespace, deployment_name, replicas):
-    api_instance = client.AppsV1Api()
-
-    # Update the deployment with the new replica count
-    update_deployment = {
-        'spec': {
-            'replicas': replicas
-        }
-    }
-
-    try:
-        api_response = api_instance.patch_namespaced_deployment(deployment_name, namespace, update_deployment)
-        print(f"Deployment {deployment_name} in namespace {namespace} has been scaled to {replicas} replicas.")
-    except client.ApiException as e:
-        print(f"Exception when calling AppsV1Api->patch_namespaced_deployment: {e}")
-
-
-@app.route("/logs", methods=["POST"])
-def logs():
-    namespace = request.form["namespace"]
-    pod_name = request.form["pod_name"]
-
-    logs = get_pod_logs(namespace, pod_name)
-    return render_template("logs.html", logs=logs)
-
-@lru_cache(maxsize=None)
-def get_pod_logs(namespace, pod_name, container_name=None):
-    api_instance = client.CoreV1Api()
-    try:
-        if container_name:
-            logs = api_instance.read_namespaced_pod_log(pod_name, namespace, container=container_name)
-        else:
-            logs = api_instance.read_namespaced_pod_log(pod_name, namespace)
-        return logs
-    except ApiException as e:
-        print(f"Exception when calling CoreV1Api->read_namespaced_pod_log: {e}")
-        return None
-
-
-@app.route("/delete-error-completed-pods", methods=["POST"])
-def delete_error_completed_pods():
-    delete_error_and_completed_pods()
-
-    # Clear the cache to force updates
-    get_deployments.cache_clear()
-    get_pods_by_deployment.cache_clear()
-    get_node_metrics.cache_clear()
-
-    return redirect("/")
-
-@lru_cache(maxsize=None)
-def delete_error_and_completed_pods():
-    api_instance = client.CoreV1Api()
-    pods = api_instance.list_pod_for_all_namespaces()
-
-    for pod in pods.items:
-        if pod.status.phase in ['Error', 'Succeeded', 'Failed']:
-            api_instance.delete_namespaced_pod(pod.metadata.name, pod.metadata.namespace)
-
-
-@app.route("/restart", methods=["POST"])
-def rollout_restart_deployment():
-    namespace = request.form["namespace"]
-    deployment_name = request.form["deployment_name"]
-
-    rollout_restart(namespace, deployment_name)
-
-    # Clear the cache to force updates
-    get_deployments.cache_clear()
-    get_pods_by_deployment.cache_clear()
-    get_node_metrics.cache_clear()
-
-    return redirect("/")
-
-@lru_cache(maxsize=None)
-def rollout_restart(namespace, deployment_name):
-    api_instance = client.AppsV1Api()
-    body = {
-        "spec": {
-            "template": {
-                "metadata": {
-                    "annotations": {
-                        "kubectl.kubernetes.io/restartedAt": datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
-                    }
-                }
-            }
-        }
-    }
-    api_instance.patch_namespaced_deployment(deployment_name, namespace, body)
-
-
 @app.route("/node_metrics")
 def node_metrics():
     node_metrics = get_node_metrics()
     node_metrics_human_readable = process_node_metrics(node_metrics)
     return jsonify(node_metrics_human_readable)
-
-
-def send_notification(message, webhook_url):
-    payload = {
-        "text": message
-    }
-    try:
-        requests.post(webhook_url, json=payload)
-    except requests.exceptions.RequestException as e:
-        print(f"Error sending notification: {e}")
-
-def check_probe_statuses(webhook_url):
-    if not webhook_url:
-        return
-
-    api_instance = client.CoreV1Api()
-    pods = api_instance.list_pod_for_all_namespaces()
-    for pod in pods.items:
-        for container_status in pod.status.container_statuses:
-            if container_status.state.waiting and container_status.state.waiting.reason != "ContainerCreating":
-                message = f"{container_status.state.waiting.reason}: {container_status.state.waiting.message}\nPod: {pod.metadata.name}, Namespace: {pod.metadata.namespace}, Container: {container_status.name}"
-                send_notification(message, webhook_url)
-
-def monitor_pod_logs(webhook_url):
-    if not webhook_url:
-        return
-
-    api_instance = client.CoreV1Api()
-    pods = api_instance.list_pod_for_all_namespaces()
-    
-    for pod in pods.items:
-        namespace = pod.metadata.namespace
-        pod_name = pod.metadata.name
-        logs = get_pod_logs(namespace, pod_name)
-        
-        if logs and "error" in logs.lower():
-            message = f"Error detected in pod logs for pod {pod_name} in namespace {namespace}"
-            send_notification(message, webhook_url)
-
-
-monitor_logs_lock = Lock()
-
-# Set your webhook URL
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
-
-scheduler = BackgroundScheduler()
-scheduler.start()
-
-scheduler.add_job(
-    func=monitor_pod_logs,
-    trigger=IntervalTrigger(minutes=1),
-    args=[SLACK_WEBHOOK_URL],  # or use TEAMS_WEBHOOK_URL for Microsoft Teams
-    replace_existing=True
-)
-
-@app.route('/stop-error-check', methods=['POST'])
-def stop_error_check():
-    # Add your code to stop the error-readiness/probness check feature
-    return redirect(url_for('index'))
-
-@app.route('/toggle-error-check', methods=['POST'])
-def toggle_error_check():
-    global error_check_enabled
-    error_check_enabled = not error_check_enabled
-    # Add your code to enable/disable the error-readiness/probness check feature accordingly
-    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
